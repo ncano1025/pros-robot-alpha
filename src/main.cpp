bdf9e31aa04e8e12293d71b1f6e22170c579b3b5
Natalia Cano <ncano1025@gmail.com>
2025-03-10 13:46:48 -0400

    added quaternion and pid code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include "main.h"
 #include "subsystems.hpp"
 
+// drive PID variables
+double drive_kP = 0.0;
+double drive_kI = 0.0;
+double drive_kD = 0.0;
+
+// turn PID variables
+bool autonomousPID = false;
+
+double turn_kP = 1.0;
+double turn_kI = 0.0;
+double turn_kD = 0.0;
+
+double heading, target;
+
+double turn_error, turn_prev_error = 0.0, turn_total_error = 0.0;
+double turn_derivative;
+
+double optimized_angle, temp_angle, turn_PID;
+
 void initialize() {
 	sensor.calibrate();
 	imu.tare_heading();
@@ -175,7 +194,6 @@ void opcontrol() {
 		// auton testing
 		if(master.getDigital(ControllerDigital::left)) {
 			autonomous();
-			//turnPID(90);
 			pros::lcd::set_text(2 , "Auto finished!");
 		}
 
@@ -231,4 +249,86 @@ void opcontrol() {
 		
 		pros::delay(20);
 	}
+}
+
+double getYawQuaternion() {
+	pros::quaternion_s_t qt = imu.get_quaternion();
+
+	//error fetching quat, retry
+	if (qt.w == PROS_ERR_F) {
+		qt = imu.get_quaternion();
+		if (qt.w == PROS_ERR_F) {
+			pros::lcd::set_text(1, "ERROR: IMU Quaternion Fetch Failed");
+			return -1.0;
+		}
+	}
+
+	//convert quat to yaw
+	double yaw = atan2(2 * ((qt.w * qt.z) + (qt.x * qt.y)), 1 - (2 * ((qt.y * qt.y) + (qt.z * qt.z)))); //yaw formula = atan2(2(wz + xy), 1 - 2(y^2 + z^2))
+
+	//returns yaw converted from rad to deg; angle is returned from -180 to 180 (+ 180 for [0, 360])
+	return ((yaw * (180 / M_PI)) + 180);
+}
+
+void turnPID(double target, double weightAdjustment) {
+	int correctCount = 0;
+	heading = getYawQuaternion();
+	pros::lcd::print(1, "Heading = %lf, Target = %lf", heading, target);
+	
+	while (correctCount <= 10) {
+		
+		heading = getYawQuaternion() - 180;
+		optimized_angle = target - heading;
+			
+		pros::lcd::print(1, "Heading = %lf, Target = %lf", heading, target);
+
+		if(target > heading){
+			temp_angle = ((360 - target) + heading) * -1;
+			if(abs(optimized_angle) > abs(temp_angle))
+				optimized_angle = temp_angle;
+		}
+		else {
+			temp_angle = (360 - heading) + target;
+			if(abs(optimized_angle) > abs(temp_angle))
+				optimized_angle = temp_angle;
+		}
+
+		// proportion
+		turn_error = optimized_angle;
+
+		pros::lcd::print(3, "Optimized = %lf", optimized_angle);
+
+		// integral
+		turn_total_error += turn_error;
+
+		// derivative
+		turn_derivative = turn_error - turn_prev_error;
+		
+		// get prev error for next instance
+		turn_prev_error = turn_error;
+
+		turn_PID = ((turn_kP * turn_error) / 360);
+		turn_PID += copysign(0.12 + (weightAdjustment * 0.05), turn_PID);
+
+		pros::lcd::print(2, "PID = %lf", turn_PID);
+
+		if(abs(turn_PID) >= 0.5)
+			drive->getModel()->tank(copysign(0.5, turn_PID), copysign(0.5, -turn_PID));
+		
+		else if(abs(optimized_angle) > 0.15)
+			drive->getModel()->tank(turn_PID, -turn_PID);
+		
+		else
+			drive->getModel()->tank(0, 0);
+		
+		if(abs(optimized_angle) <= 0.2)
+			correctCount++;
+		
+		pros::delay(10);
+	}
+
+	drive->getModel()->tank(0, 0);
+
+	pros::delay(250);
+	
 }
